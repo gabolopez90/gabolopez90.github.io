3673a620d0c160e62db114faeca0f4edbfa93b6a
Gabriel López <gabolopez1990@gmail.com>
2017-02-21 16:38:19 -0400

    Add files via upload
    Burger icon to an X

--- javascript.js
+++ javascript.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
     $("#header, #sobreMi, #habilidades, #portafolio, #footer, #bot, hr").fadeTo("slow",1);
     $(this).fadeOut('slow');
   });
+  //Burger to X toggle
+  $(".navbar-toggle").click(function(){
+    $(this).toggleClass("open");
+  });
   //Smooth scrolling
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
